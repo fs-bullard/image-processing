c7aa68beb372550dce7df7802a0565293ce2a46e
fs-bullard <42214857+fs-bullard@users.noreply.github.com>
2022-08-05 19:32:12 +0100

    added edges.py

---
+++ edges.py
@@ -0,0 +1,17 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+import math
+from PIL import Image
+
+def laplacianEdge():
+    pass
+
+def cannyEdge():
+    pass
+
+def prewittEdge():
+    pass
+
+def sobelEdge():
+    pass
