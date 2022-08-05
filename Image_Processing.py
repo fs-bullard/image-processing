d0c1feb0c9857e8bc291aefac75fae6e6630b7b8
fs-bullard <42214857+fs-bullard@users.noreply.github.com>
2022-08-05 18:24:45 +0100

    Made small improvements to 1 pass rgb, generally tidied up code

--- Image_Processing.py
+++ Image_Processing.py
@@ -8,6 +8,10 @@ from PIL import Image
 
 def gaussianBlur_1pass_bw(sigma, img_name):
     '''
+    Inputs:
+    sigma: Standard deviation of the normal distribution
+    img_name: filename of input image
+    Returns: Blurred greyscale image as nparray
     '''
     def gauss_func_2D(sigma, x, y):
             """
@@ -30,23 +34,20 @@ def gaussianBlur_1pass_bw(sigma, img_name):
         for j in range(2*radius+1):
             # Set the (i,j)th element to the value of Gauss fn at this point
             kernel[i,j] = gauss_func_2D(sigma, i-radius, j-radius)
+            # Add value of Gauss fn at this point to kernel sum
             kernelSum += gauss_func_2D(sigma, i-radius, j-radius)
-    print(kernel)
-    print(f'Kernel sum = {kernelSum}')
-    
-
+    # Open image and convert to greyscale numpy array
     img = np.asarray(Image.open(img_name).convert('L'))
-    # plt.imshow(img, cmap='gray')
-    # plt.show()
-
-    img_out = signal.convolve2d(img, kernel, boundary='symm', mode='same')
-
-
-    plt.imshow(img_out, cmap='gray')
-    plt.show()
+    # Convolve image with Gaussian kernel and divide by kernelSum to retain brightness
+    img_out = signal.convolve2d(img, kernel, boundary='symm', mode='same') / kernelSum
+    return img_out
 
 def gaussianBlur_1pass_rgb(sigma, img_name):
     '''
+    Inputs:
+    sigma: Standard deviation of the normal distribution
+    img_name: filename of input image
+    Returns: Blurred RGB image as nparray
     '''
     def gauss_func_2D(sigma, x, y):
             """
@@ -69,36 +70,22 @@ def gaussianBlur_1pass_rgb(sigma, img_name):
         for j in range(2*radius+1):
             # Set the (i,j)th element to the value of Gauss fn at this point
             kernel[i,j] = gauss_func_2D(sigma, i-radius, j-radius)
+            # Add value of gauss function at i,j to kernelSum
             kernelSum += gauss_func_2D(sigma, i-radius, j-radius)
-    print(kernel)
-    print(f'Kernel sum = {kernelSum}')
-    
-
-    img = np.asarray(Image.open(img_name).convert('RGB'))
-    img_r = img[:,:,0]
-    img_g = img[:,:,1]
-    img_b = img[:,:,2]
-    # plt.imshow(img, cmap='gray')
-    # plt.show()
-
-    img_out = img.copy()
-
+    # Load image and convert to numpy array
+    img = np.asarray(Image.open(img_name))
+    # Create an empty image as numpy array
+    img_out = np.zeros(np.shape(img), dtype=np.uint8)
+    # For each channel, convolve with the kernel and divide by kernelSum to retain brightness
     for i in range(3):
-        img_out[:,:,i] = signal.convolve2d(img[:,:,i], kernel, boundary='symm', mode='same')
-
-    # img_r_out = signal.convolve2d(img_r, kernel, boundary='symm', mode='same')
-    # img_g_out = signal.convolve2d(img_g, kernel, boundary='symm', mode='same')
-    # img_b_out = signal.convolve2d(img_b, kernel, boundary='symm', mode='same')
-
-    # img_out = (np.dstack((img_r_out, img_g_out, img_b_out))*255.999).astype(np.uint8)
-    
-
-
-    plt.imshow(img_out)
-    plt.show()
+        img_out[:,:,i] = signal.convolve2d(img[:,:,i], kernel, boundary='symm', mode='same') / kernelSum   
+    return img_out
 
 def gaussianBlur_2pass_rgb(sigma, img_name):
     '''
+    sigma: Standard deviation of the normal distribution
+    img_name: filename of input image
+    Returns: Blurred RGB image as nparray
     '''
     def gauss_func_1D(sigma, x):
             """
@@ -107,7 +94,6 @@ def gaussianBlur_2pass_rgb(sigma, img_name):
             Returns: value of Gaussian function at x from the center
             """
             return (np.sqrt(1/(2*np.pi*sigma**2))) * np.exp((-x**2)/(2*sigma**2))
-
     # --------------- Build Gaussian matrix ------------------ #
     # Define radius as ceil(3 * sigma)
     radius = math.ceil(3*sigma)
@@ -119,45 +105,42 @@ def gaussianBlur_2pass_rgb(sigma, img_name):
     for i in range(2*radius+1):
         # Set the (i,j)th element to the value of Gauss fn at this point
         kernel_1[i] = gauss_func_1D(sigma, i-radius)
+        # Add value of gauss fn at i,j to kernelSum
         kernelSum += gauss_func_1D(sigma, i-radius)
+    # Set second kernel as the transpose of the first
     kernel_2 = np.transpose(kernel_1)
-    print(kernel_1)
-    print(kernel_2)
-    print(f'Kernel sum = {kernelSum}')
-
-    
-
-    img = np.asarray(Image.open(img_name).convert('RGB'))
-    img_r = img[:,:,0]
-    img_g = img[:,:,1]
-    img_b = img[:,:,2]
-    # plt.imshow(img, cmap='gray')
-    # plt.show()
-
-    img_out = img.copy()
+    # Load image and convert to numpy array
+    img = np.asarray(Image.open(img_name))
+    # Create an empty image as numpy array
+    img_out = np.zeros(np.shape(img), dtype=np.uint8)
 
     for i in range(3):
-        img_out[:,:,i] = signal.convolve2d(img[:,:,i], kernel_1, boundary='symm', mode='same')
-        img_out[:,:,i] = signal.convolve2d(img_out[:,:,i], kernel_2, boundary='symm', mode='same')
+        img_out[:,:,i] = signal.convolve2d(img[:,:,i], kernel_1, boundary='symm', mode='same') / kernelSum
+        img_out[:,:,i] = signal.convolve2d(img_out[:,:,i], kernel_2, boundary='symm', mode='same') / kernelSum 
 
     # img_r_out = signal.convolve2d(img_r, kernel, boundary='symm', mode='same')
     # img_g_out = signal.convolve2d(img_g, kernel, boundary='symm', mode='same')
     # img_b_out = signal.convolve2d(img_b, kernel, boundary='symm', mode='same')
 
     # img_out = (np.dstack((img_r_out, img_g_out, img_b_out))*255.999).astype(np.uint8)
-    
 
-
-    plt.imshow(img_out)
-    plt.show()
+    return img_out
 
 
 e1 = cv2.getTickCount()
-# gaussianBlur_1pass_rgb(3, 'elcapitan.jpg') # Uncomment for rgb gaussian in 1 pass
-# gaussianBlur_1pass_bw(3, 'el_capitan.jpg') # Uncomment for greyscale gaussian in 1 pass
-gaussianBlur_2pass_rgb(3, 'elcapitan.jpg') # Uncomment for rgb gaussian in 2 passes
+# ------ Uncomment below for greyscale gaussian in 1 pass ------ #
+# img = gaussianBlur_1pass_bw(3, 'el_capitan.jpg') 
+# ------ Uncomment below for rgb gaussian in 1 pass ------- #
+# img = gaussianBlur_1pass_rgb(3, 'elcapitan.jpg') 
+# ------ Uncomment below for rgb gaussian in 2 passes ------ #
+img = gaussianBlur_2pass_rgb(3, 'elcapitan.jpg') 
 
+# Track how long the program takes
 e2 = cv2.getTickCount()
 progTime = (e2 - e1) / cv2.getTickFrequency()
 print(f'Time = {progTime}')
 print(f'Ticks: {e2 - e1}')
+
+# Show image with matplotlib
+plt.imshow(img)
+plt.show()
