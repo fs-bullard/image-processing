e36e23ffdce3254d27bbe841debfcb698e1a7056
fs-bullard <42214857+fs-bullard@users.noreply.github.com>
2022-08-05 18:45:45 +0100

    switched from convolve2D to cv2.filter2D to decrease runtime

--- Image_Processing.py
+++ Image_Processing.py
@@ -78,7 +78,7 @@ def gaussianBlur_1pass_rgb(sigma, img_name):
     img_out = np.zeros(np.shape(img), dtype=np.uint8)
     # For each channel, convolve with the kernel and divide by kernelSum to retain brightness
     for i in range(3):
-        img_out[:,:,i] = signal.convolve2d(img[:,:,i], kernel, boundary='symm', mode='same') / kernelSum   
+        img_out[:,:,i] = cv2.filter2D(img[:,:,i], -1, kernel) / kernelSum   
     return img_out
 
 def gaussianBlur_2pass_rgb(sigma, img_name):
@@ -98,31 +98,28 @@ def gaussianBlur_2pass_rgb(sigma, img_name):
     # Define radius as ceil(3 * sigma)
     radius = math.ceil(3*sigma)
     # Create array of zeros dimension (2* radius + 1, 2*radius + 1)
-    kernel_1 = np.zeros(2*radius + 1)
+    kernel = np.zeros(2*radius + 1)
+    print(kernel)
+    print('-------------------')
+    print(np.transpose(kernel))
+    print('-------------------')
+    print(kernel.T)
     # Set kernel sum to 0
     kernelSum = 0.0
     # Iterate i from -radius to radius inclusive
     for i in range(2*radius+1):
         # Set the (i,j)th element to the value of Gauss fn at this point
-        kernel_1[i] = gauss_func_1D(sigma, i-radius)
+        kernel[i] = gauss_func_1D(sigma, i-radius)
         # Add value of gauss fn at i,j to kernelSum
         kernelSum += gauss_func_1D(sigma, i-radius)
-    # Set second kernel as the transpose of the first
-    kernel_2 = np.transpose(kernel_1)
     # Load image and convert to numpy array
     img = np.asarray(Image.open(img_name))
     # Create an empty image as numpy array
     img_out = np.zeros(np.shape(img), dtype=np.uint8)
 
     for i in range(3):
-        img_out[:,:,i] = signal.convolve2d(img[:,:,i], kernel_1, boundary='symm', mode='same') / kernelSum
-        img_out[:,:,i] = signal.convolve2d(img_out[:,:,i], kernel_2, boundary='symm', mode='same') / kernelSum 
-
-    # img_r_out = signal.convolve2d(img_r, kernel, boundary='symm', mode='same')
-    # img_g_out = signal.convolve2d(img_g, kernel, boundary='symm', mode='same')
-    # img_b_out = signal.convolve2d(img_b, kernel, boundary='symm', mode='same')
-
-    # img_out = (np.dstack((img_r_out, img_g_out, img_b_out))*255.999).astype(np.uint8)
+        img_out[:,:,i] = cv2.filter2D(cv2.filter2D(img[:,:,i], -1 ,  kernel) / kernelSum , -1 , np.transpose(kernel)) / kernelSum
+        # img_out[:,:,i] = signal.convolve(img_out[:,:,i], kernel_2, mode='same') / kernelSum 
 
     return img_out
 
@@ -131,9 +128,9 @@ e1 = cv2.getTickCount()
 # ------ Uncomment below for greyscale gaussian in 1 pass ------ #
 # img = gaussianBlur_1pass_bw(3, 'el_capitan.jpg') 
 # ------ Uncomment below for rgb gaussian in 1 pass ------- #
-# img = gaussianBlur_1pass_rgb(3, 'elcapitan.jpg') 
+img = gaussianBlur_1pass_rgb(3, 'elcapitan.jpg') 
 # ------ Uncomment below for rgb gaussian in 2 passes ------ #
-img = gaussianBlur_2pass_rgb(3, 'elcapitan.jpg') 
+# img = gaussianBlur_2pass_rgb(1, 'elcapitan.jpg') 
 
 # Track how long the program takes
 e2 = cv2.getTickCount()
