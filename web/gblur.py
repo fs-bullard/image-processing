cfbc1e61446598eefde7d677f47e96a218c05736
fs-bullard <42214857+fs-bullard@users.noreply.github.com>
2022-08-14 01:48:31 +0100

    Set up Google Cloud authentication

--- web/gblur.py
+++ web/gblur.py
@@ -4,11 +4,11 @@ import math
 from PIL import Image
 import os
 
-def gaussRGB(sigma, img_name):
+def gaussRGB(sigma, img):
     '''
     sigma: Standard deviation of the normal distribution
-    img_name: filename of input image
-    Returns: Blurred RGB image as nparray
+    img: input image
+    Returns: Blurred RGB image 
     '''
     def gauss_func_1D(sigma, x):
             """
@@ -17,21 +17,27 @@ def gaussRGB(sigma, img_name):
             Returns: value of Gaussian function at x from the center
             """
             return (np.sqrt(1/(2*np.pi*sigma**2))) * np.exp((-x**2)/(2*sigma**2))
+    
     # --------------- Build Gaussian matrix ------------------ #
     # Define radius as ceil(3 * sigma)
     radius = math.ceil(3*sigma)
+
     # Create array of zeros dimension (2* radius + 1, 2*radius + 1)
     kernel = np.zeros((2*radius + 1,1))
+
     # Set kernel sum to 0
     kernelSum = 0.0
+
     # Iterate i from -radius to radius inclusive
     for i in range(2*radius+1):
         # Set the (i,j)th element to the value of Gauss fn at this point
         kernel[i] = gauss_func_1D(sigma, i-radius)
+
         # Add value of gauss fn at i,j to kernelSum
         kernelSum += gauss_func_1D(sigma, i-radius)
+
     # Load image and convert to numpy array
-    img = np.asarray(Image.open(img_name))
+    img = np.asarray(img)
     # Create an empty image as numpy array
     img_out = np.zeros(np.shape(img), dtype=np.uint8)
 
@@ -39,8 +45,6 @@ def gaussRGB(sigma, img_name):
         img_out[:,:,i] = cv2.filter2D(cv2.filter2D(img[:,:,i], -1 ,  kernel) / kernelSum , -1 , kernel.T ) / kernelSum
         # img_out[:,:,i] = signal.convolve(img_out[:,:,i], kernel_2, mode='same') / kernelSum 
 
-    # Save Image
+    # Return image from array
     output = Image.fromarray(img_out, mode="RGB")
-    # output.save(os.path.splitext(img_name)[0] + "." + Image.open(img_name).format)
-    
-    return os.path.splitext(img_name)[0] + "." + Image.open(img_name).format, output
+    return output
--- web/main.py
+++ web/main.py
@@ -3,15 +3,18 @@ from flask import Flask
 from flask import request, escape, render_template
 from gblur import gaussRGB
 from werkzeug.utils import secure_filename
-import os
+import io, os
 from google.cloud import storage
+import random
+from PIL import Image
 
 ALLOWED_EXTENSIONS = set(['.png', '.jpg', '.jpeg'])
 
 app = Flask(__name__)
 
-# Configure this environment variable via app.yaml
-CLOUD_STORAGE_BUCKET = os.environ['img-proc-fb']
+# # Configure enviroment variable via app.yaml
+# CLOUD_STORAGE_BUCKET = os.environ['CLOUD_STORAGE_BUCKET']
+
 
 @app.route("/")
 def index():   
@@ -21,16 +24,35 @@ def index():
 def get_image():
     if request.method == "POST":
         f = request.files['file']
-        # Create a cloud storage client
-        gcs = 
-        sfname = 'static/images/'+str(secure_filename(f.filename))
-        print(os.path.splitext(sfname)[1])
-        if os.path.splitext(sfname)[1] in ALLOWED_EXTENSIONS:
-            f.save(sfname)
-            sigma = 5
-            bname = gaussRGB(sigma, sfname)
+        f_name = secure_filename(f.filename)
 
-            return render_template('result.html', title='Result', imgpath=bname)
+        # Create a cloud storage client
+        client = storage.Client.from_service_account_json('balmy-nuance-359122.json')
+        
+        # Set image hash
+        im_hash = random.randint(1,9999999)
+
+        # Get the bucket that the image will be uploaded to
+        bucket = client.get_bucket('img-proc-fb')
+
+        sfname = 'Uploaded image' + str(im_hash) + os.path.splitext(f_name)[1]
+ 
+
+        if os.path.splitext(f_name)[1] in ALLOWED_EXTENSIONS:
+            # Create new blob and upload the original image
+            in_blob = bucket.blob(sfname)
+            in_blob.upload_from_file(f)
+
+            sigma = 5
+            imin_b = in_blob.download_as_bytes()
+            imin = Image.frombytes('RGBA', f.shape(),imin_b)
+            imout = gaussRGB(sigma, imin)
+            
+            # Create a new blob and upload blurred image
+            out_blob = bucket.blob('blur-' + sfname)
+            out_blob.upload_from_file(imout)
+
+            return render_template('result.html', title='Result', img=out_blob.media_link)
         else:
             return render_template('load.html', title='Upload Image', content="Incorrect Filetype, must be .png, .jpg or .jpeg.")
     
