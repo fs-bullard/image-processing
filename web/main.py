05980110a7a3f9f8fbd90308293eab222bc85ce7
fs-bullard <42214857+fs-bullard@users.noreply.github.com>
2022-08-16 22:57:07 +0100

    sorted out pages and improved frontend

--- web/main.py
+++ web/main.py
@@ -1,6 +1,5 @@
-from tkinter import ALL
-from flask import Flask
-from flask import request, escape, render_template
+from flask import Flask, send_file
+from flask import request, escape, render_template, session
 from flask_bootstrap import Bootstrap5
 
 from gblur import gaussRGB
@@ -8,6 +7,8 @@ from werkzeug.utils import secure_filename
 import io, os
 from google.cloud import storage
 import random
+import requests
+from PIL import Image
 
 
 
@@ -18,18 +19,24 @@ app = Flask(__name__)
 bootstrap = Bootstrap5(app)
 app.config['BOOTSTRAP_BOOTSWATCH_THEME'] = 'darkly'
 
+app.secret_key = '5fff128d0b5405ec4d946d3033090ed247bc9d10112be9ef'
 
 @app.route("/")
 def index():   
-    return render_template('load.html', title='tooNOISY', content="Upload image to blur")
+    return render_template('index.html', title='tooNOISY')
 
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
 
-@app.route("/uploader", methods=["GET", "POST"])
+@app.route("/reduce")
+def reduce():
+    return render_template('load.html', title="Noise Removal")
+
+@app.route("/load", methods=["GET", "POST"])
 def get_image():
     if request.method == "POST":
+        # Load image
         f = request.files['file']
         f_name = secure_filename(f.filename)
 
@@ -42,34 +49,78 @@ def get_image():
         # Get the bucket that the image will be uploaded to
         bucket = client.get_bucket('img-proc-fb')
 
-        sfname = 'Uploaded image' + str(im_hash) + os.path.splitext(f_name)[1]
+        # Create new name for the image
+        sfname = 'Uploaded image' + str(im_hash) + os.path.splitext(f_name)[-1]
+        # Save to session
+        session['og_img'] = sfname
         
-
+        # If the file is jpeg or png
         if os.path.splitext(f_name)[1] in ALLOWED_EXTENSIONS:
             # Create new blob and upload the original image
-            # in_blob = bucket.blob(sfname)
-            # in_blob.upload_from_file(f)
-            sigma = 5
-            imout = gaussRGB(sigma, f)
-            # imout.save()
-            # Close original image
+            in_blob = bucket.blob(sfname)
+            in_blob.upload_from_file(f)
             f.close()
-            
-            # Create a new blob and upload blurred image
-            out_blob = bucket.blob('blur-' + sfname)
-            buffer = io.BytesIO()
-            imout.save(buffer, format='JPEG')
-            out_blob.upload_from_string(buffer.getvalue(), "image/jpeg")
-            
-            # Close blurred image
-            imout.close()
-
-            return render_template('result.html', title='Result', img=out_blob.media_link)
+            # Return page displaying input image
+            return render_template('img_loaded.html', title='toonoisy', img=in_blob.media_link)
         else:
             return render_template('load.html', title='Upload Image', content="Incorrect Filetype, must be .png, .jpg or .jpeg.")
     
+
+@app.route("/result", methods=["GET", "POST"])
+def noise_reduce():
+    if request.method == "POST":
+        # Create a cloud storage client
+        client = storage.Client.from_service_account_json('balmy-nuance-359122.json')
+
+        # Get the bucket 
+        bucket = client.get_bucket('img-proc-fb')
+
+        # Download the original image from the bucket
+        data = requests.get(bucket.get_blob(session['og_img']).media_link).content
+        f = io.BytesIO(data)
+
+        sigma = 5
+        imout = gaussRGB(sigma, f)
+        f.close()
+
+        # Create a new blob and upload blurred image
+        blur_name = 'blur-' + session['og_img']
+        out_blob = bucket.blob(blur_name)
+        buffer = io.BytesIO()
+        imout.save(buffer, format='JPEG')
+        out_blob.upload_from_string(buffer.getvalue(), "image/jpeg")
         
-        
+        # Close blurred image
+        imout.close()
+        return render_template('result.html', title='Result', img=out_blob.media_link)
+
+@app.route("/save")
+def save_result():
+    print(20000000000000000000000000090000000000000000)
+    # Create a cloud storage client
+    client = storage.Client.from_service_account_json('balmy-nuance-359122.json')
+
+    # Get the bucket 
+    bucket = client.get_bucket('img-proc-fb')
+
+    f_name = 'blur-' + session['og_img']
+    data = Image.open(bucket.get_blob(f_name).media_link).content
+    # f = io.BytesIO(data)
+    # img = Image.open(f)
+    buffer = io.BytesIO()
+    return send_file(
+        data.save(buffer, 'JPEG'),
+        as_attachment=True,
+        download_name='noisereduced' + str(random.randint(1,1000)),
+        mimetype='image/jpeg'
+        )
+
+
+# Google verification
+@app.route("/googleb5e42b25019c3d31.html")
+def verify_site():
+    return render_template('googleb5e42b25019c3d31.html')
+
 
 
 if __name__ == "__main__":
