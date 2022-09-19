afcfa7a94913fdc62e144af875cc5c977240dab1
fs-bullard <42214857+fs-bullard@users.noreply.github.com>
2022-09-19 19:26:21 +0100

    streamlined and added test images

--- web/main.py
+++ web/main.py
@@ -1,9 +1,9 @@
-from flask import Flask, send_file
-from flask import request, escape, render_template, session
+from flask import Flask
+from flask import request, render_template, session
 from flask_bootstrap import Bootstrap5
 
 from gblur import gauss
-from medianfilt import median_filter
+from medianfilt import median_filter, fast_median_filter
 from werkzeug.utils import secure_filename
 import io, os, gc
 from google.cloud import storage
@@ -35,16 +35,12 @@ ALLOWED_EXTENSIONS = set(['.png', '.jpg', '.jpeg'])
 
 
 # ----------------- Helper functions ------------------------ #
-def set_cloud_storage():
-    ''''''
-    # Create a cloud storage client
-    client = storage.Client(project='toonoisy')
 
-    # Get the bucket 
-    bucket = client.get_bucket(CLOUD_STORAGE_BUCKET)
+# Create a cloud storage client
+client = storage.Client(project='toonoisy')
 
-    return bucket
-
+# Get the bucket 
+bucket = client.get_bucket(CLOUD_STORAGE_BUCKET)
 
 
 # ----------------- Routing Functions ------------------------ #
@@ -66,11 +62,7 @@ def get_image():
     if request.method == "POST":
         # Load image
         f = request.files['file']
-        f_name = secure_filename(f.filename)
-
-        # Get cloud storage bucket
-        bucket = set_cloud_storage()
-        
+        f_name = secure_filename(f.filename)     
         # Set image hash
         im_hash = random.randint(1,9999999)
 
@@ -82,39 +74,59 @@ def get_image():
         # If the file is jpeg or png
         if os.path.splitext(f_name)[1] in ALLOWED_EXTENSIONS:
             # Create new blob and upload the original image
+            # Convert to grayscale
             in_blob = bucket.blob(sfname)
-            in_blob.upload_from_file(f)
-            del f
+            img = Image.open(f).convert('L')
+            buffer = io.BytesIO()
+            img.save(buffer, format='JPEG')
+            in_blob.upload_from_string(buffer.getvalue(), "image/jpeg")
+            del f, img
             gc.collect()
             # Return page displaying input image
             return render_template('img_loaded.html', title='toonoisy', img=in_blob.media_link)
         else:
             return render_template('load.html', title='Upload Image', content="Incorrect Filetype, must be .png, .jpg or .jpeg.")
 
+@app.route("/barbara", methods=["GET", "POST"])
+def example_barbara():
+    # Create filename
+    im_hash = random.randint(1,9999999)
+    sfname = 'og_' + str(im_hash) + 'barbara.png'
+    session['og_img'] = sfname
+    
+    # Duplicate barbara image with new name
+    barbara = bucket.blob('barbara.png')
+    bucket.copy_blob(barbara, bucket, sfname)
+    return render_template('img_loaded.html', title='toonoisy', img="https://storage.cloud.google.com/toonoisy_ims/barbara.png")
+
+@app.route("/kodim", methods=["GET", "POST"])
+def example_kodim():
+    # Get cloud storage bucket
+    # bucket = set_cloud_storage()
+    # Create filename
+    im_hash = random.randint(1,9999999)
+    sfname = 'og_' + str(im_hash) + 'kodim.jpg'
+    session['og_img'] = sfname
+    
+    # Duplicate barbara image with new name
+    barbara = bucket.blob('kodim.jpg')
+    bucket.copy_blob(barbara, bucket, sfname)
+    return render_template('img_loaded.html', title='toonoisy', img="https://storage.cloud.google.com/toonoisy_ims/kodim.jpg")
+
+
 @app.route('/reload')
 def restart_with_same():
-    # Set up cloud storage bucket
-    bucket = set_cloud_storage()
     img_og = bucket.get_blob(session['og_img'])
     return render_template('img_loaded.html', title="toonoisy", img=img_og.media_link)
 
 @app.route("/gauss", methods=["GET", "POST"])
 def gauss_reduce():
-    if request.method == "POST":
-        
-        # Setup cloud storage client and bucket
-        bucket = set_cloud_storage()
-
-        # Download the original image from the bucket
+    if request.method == "POST":        # Download the original image from the bucket
         data = requests.get(bucket.get_blob(session['og_img']).media_link).content
         f = io.BytesIO(data)
-        del data
-        gc.collect()
 
         sigma = float(request.form['sigma'])
         imout = gauss(sigma, f)
-        del f
-        gc.collect()
 
         # Create a new blob and upload blurred image
         blur_name = 'blur-' + session['og_img']
@@ -124,30 +136,19 @@ def gauss_reduce():
         out_blob.upload_from_string(buffer.getvalue(), "image/jpeg")
         
         # Close blurred image
-        del imout
-        del buffer
+        del buffer, imout, f, data
         gc.collect()
         return render_template('result.html', title='Gauss', img=out_blob.media_link)
 
 @app.route("/median", methods=["GET", "POST"])
 def median_reduce():
     if request.method == "POST":
-        
-        # Setup cloud storage client and bucket
-        bucket = set_cloud_storage()
-
-
         # Download the original image from the bucket
         data = requests.get(bucket.get_blob(session['og_img']).media_link).content
         f = io.BytesIO(data)
 
-        del data
-        gc.collect()
-
         width = int(request.form['width'])
-        imout = median_filter(f, width)
-        del f
-        gc.collect()
+        imout = fast_median_filter(f, width)
 
         # Create a new blob and upload blurred image
         blur_name = 'blur-' + session['og_img']
@@ -157,20 +158,24 @@ def median_reduce():
         out_blob.upload_from_string(buffer.getvalue(), "image/jpeg")
         
         # Close blurred image
-        del imout
-        del buffer
+        del buffer, imout, data, f
         gc.collect()
         
         return render_template('result.html', title='Median', img=out_blob.media_link)
 
 @app.route('/compare')
 def sidebyside():
-    # Set up cloud storage bucket
-    bucket = set_cloud_storage()
     img_og = bucket.get_blob(session['og_img'])
     img_new = bucket.get_blob('blur-' + session['og_img'])
     
     return render_template('sidebyside.html', title='Compare', img_1=img_og.media_link, img_2=img_new.media_link)
+
+@app.route('/sharpen')
+def sharpen():
+    pass
+
+
+
 
 @app.errorhandler(500)
 def internal_server_error():
