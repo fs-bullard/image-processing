26692de7e5059bf1aba1d4358ea94c3c7aba179a
fs-bullard <42214857+fs-bullard@users.noreply.github.com>
2022-09-19 23:41:08 +0100

    initial commit

---
+++ non-web-files/bilateral.py
@@ -0,0 +1,17 @@
+import numpy as np
+from PIL import Image
+
+
+
+
+
+
+if __name__ == '__main__':
+    img = Image.open('non-web-files/kodim.jpg')
+    
+
+
+
+
+    img.show()
+    # img_out.show()
