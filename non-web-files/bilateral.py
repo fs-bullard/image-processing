2c58e86df17209c1a75332d238ba0971636813e1
fs-bullard <freddie.bullard@outlook.com>
2022-09-21 19:02:13 +0100

    image cropped

--- non-web-files/bilateral.py
+++ non-web-files/bilateral.py
@@ -24,22 +24,22 @@ def denoise_pixel(window:np.ndarray, sigd:int, sigr:int):
     # print(wp, rest)
     return int(rest / wp)
 
-def dumb_bilateral(img:cv.Mat, d, sigd, sigr):
+def dumb_bilateral(img:cv.Mat, radius, sigd, sigr):
     print('Applying dumb bilateral filter')
     h, w = img.shape
-    img_out = np.zeros((h,w), dtype=np.uint8)
-    radius = d
+    img_out = np.zeros((h, w), dtype=np.uint8)
     
-    x = radius // 2 
-    while x < w - radius // 2:
-        y = radius // 2
-        while y < h - radius // 2:
+    border = radius // 2 
+    x = border
+    while x < w - border:
+        y = border
+        while y < h - border:
             # print(f'(x, y) : {(x, y)}')
             # Build window
             winx, winy = 0, 0
             window = np.zeros((radius, radius), dtype=np.uint8)
-            for i in range(x - radius // 2, x + radius // 2 + 1):
-                for j in range(y - radius // 2, y + radius // 2 + 1):
+            for i in range(x - border, x + border + 1):
+                for j in range(y - border, y + border + 1):
                     window[winy % radius, winx % radius] = img[j, i]
                     winy += 1
                 winx += 1
@@ -47,17 +47,17 @@ def dumb_bilateral(img:cv.Mat, d, sigd, sigr):
             y += 1
         x += 1
 
-    return img_out.astype(np.uint8)
+    return img_out[border:-border,border:-border].astype(np.uint8)
 
 if __name__ == '__main__':
     print('------------ Processing ---------------')
 
-    img = cv.imread('anglicised-rat.jpg', cv.IMREAD_GRAYSCALE)
+    img = cv.imread('barbara.png', cv.IMREAD_GRAYSCALE)
 
-    img_out = dumb_bilateral(img, d=3, sigd=150, sigr=150)
+    img_out = dumb_bilateral(img, radius=3, sigd=150, sigr=150)
     img_eg = cv.bilateralFilter(img, d=3, sigmaColor=150, sigmaSpace=150)
     fig1 = plt.figure('mine')
     plt.imshow(img_out, 'gray')
     fig2 = plt.figure('not mine')
-    plt.imshow(img_eg, 'gray')
+    plt.imshow(img, 'gray')
     plt.show()
